bc2a93abcf1329ec9bf3f6ab90ab84b29fa77aa1
Taylor Woodcock <taylor.woodcock@outlook.com>
2018-10-18 11:34:57 +0100

    Update energy.cpp

--- energy.cpp
+++ energy.cpp
@@ -1,1 +1,65 @@
+#include "pxt.h"
 
+using namespace pxt;
+
+#include "MicroBit.h"
+
+#define SAMPLES 250
+#define HISTORY 5
+#define RANGE_MIN 3607
+#define RANGE_MAX 256981
+#define WATTAGE_MAX 3000
+
+namespace energy {
+  int samples[SAMPLES];
+  int history[HISTORY];
+  
+  int amplitude;
+  int watts;
+
+  int sampleAmplitude(int samples[SAMPLES])
+  {
+    int peak = 0;
+    int trough = 0;
+    
+    for(int i = 0; i < SAMPLES; i++)
+    {
+      if(samples[i] > peak)
+        peak = samples[i];
+  
+      if(samples[i] < trough || trough == 0)
+        trough = samples[i];
+    }
+    return peak - trough;
+  }
+  
+  /**
+   * Maps a value from one range to another.
+   *
+   * @param value The value from the original 
+   */
+  int map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+  {
+    int val = ((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow) + toLow;
+    if(val < 0)
+      return 0;
+    return val;
+  }
+  
+  int getEnergyUsage()
+  {
+    int period = uBit.compass.getPeriod();
+    uBit.compass.setPeriod(1);
+    
+    for(int sample = 0; sample < SAMPLES; sample++)
+    {
+      samples[sample] = uBit.compass.getFieldStrength(); 
+    }
+    
+    int amplitude = sampleAmplitude(samples);
+    int watts = map(amplitude, RANGE_MIN, RANGE_MAX, 0, WATTAGE_MAX);
+    
+    return watts;
+  }
+  
+}
